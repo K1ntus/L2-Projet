d979e8ea53ff2cb96065bafe2f6489a43ace0b20
Jordan <masson.jordan@gmail.com>
2018-03-01 14:48:46 +0100

    wololo

--- solver_jojo.c
+++ solver_jojo.c
@@ -6,6 +6,11 @@
 #include "game_display.c"
 #include "game_io.h"
 
+
+typedef enum e_solve_mode {FIND_ONE,NB_SOL,FIND_ALL} solve_mode;
+
+
+
 bool potential_invalid_game(game g){
   for(unsigned int x = 0; x < game_width(g);x++){
     if(required_nb_seen(g, N, x) - current_nb_seen(g,N,x) < 0)
@@ -35,7 +40,7 @@ bool board_is_full(game g){
 }
 
 
-game is_valid(game g, int pos, content monster){
+game is_valid(game g, int pos, content monster, int *nb_sol){
   int x = pos/game_width(g);
   int y = pos/game_height(g);
 
@@ -54,6 +59,7 @@ game is_valid(game g, int pos, content monster){
 
   if(board_is_full(g2)){
     if(is_game_over(g2)){
+      nb_sol +=1;
       return g2;
     }else{
       free(g2);
@@ -61,21 +67,24 @@ game is_valid(game g, int pos, content monster){
     }
   }
 
-  return is_valid(g2, pos++, ZOMBIE);
-  return is_valid(g2, pos++, GHOST);
-  return is_valid(g2, pos++, SPIRIT);
-  return is_valid(g2, pos++, VAMPIRE);
+  return is_valid(g2, pos++, ZOMBIE, nb_sol);
+  return is_valid(g2, pos++, GHOST, nb_sol);
+  return is_valid(g2, pos++, SPIRIT, nb_sol);
+  return is_valid(g2, pos++, VAMPIRE, nb_sol);
 }
 
 
 int main(void) {
   game g1 = load_game("autosave");
-  g1 = is_valid(g1,0,EMPTY);
+  int nb_solution = 0;
+
+  g1 = is_valid(g1,0,EMPTY, &nb_solution);
 
   //display(g);
   if(g1 == NULL){
     printf("No solution has been find\n");
   } else{
+    printf("There's %d solutions! \n",nb_solution);
     save_game(g1, "solution_dude");
   }
   free(g1);
