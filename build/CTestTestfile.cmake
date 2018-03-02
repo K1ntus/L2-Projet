f8401b4592a7039f9f7cfb980d7a2da39508a487
Jordane Masson <masson.jordan@gmail.com>
2018-03-02 15:36:37 +0100

    oups i pushed my build folder

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /autofs/netapp/travail/jmasson/Année/L2/Semestre 4/workspace/Undead-V2
-# Build directory: /autofs/netapp/travail/jmasson/Année/L2/Semestre 4/workspace/Undead-V2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_output "test_exec" "COMMAND" "valgrind" "--tool=memcheck" "--log-file=log.txt" "--leak-check=full" "--show-reachable=yes" "--error-exitcode=2" "$<TARGET_FILE:test_exec>")
